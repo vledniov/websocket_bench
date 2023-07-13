0512e5e94cbeb38318151c2e1dfeea728efc8d52
vledniov <vledniov@mixbook.com>
2023-07-13 13:25:00 +0300

    Some more minor adjustments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://', nil]
+
+  config.log_level = :warn
+  config.hosts.clear
 end
